b05136f66f2af69025afa8a3ae95763b8e701a07
Pulie <pverapavez@gmail.com>
2020-05-11 20:43:01 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Button } from 'antd'
 
 function App() {
   return (
     <div>
       <h1>Ahora Nosotras</h1>
+      <Button type="primary">hola</Button>
     </div>
   );
 }
