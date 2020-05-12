14090fe203ceffbbfe93a914f87e7f654d0c32c7
Pulie <pverapavez@gmail.com>
2020-05-11 20:58:57 -0400

    firebase ok

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+// Config Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyArGrICHpjY41J-3P1xllzYht8uctbiDo8",
+  authDomain: "ahoranosotrasrd.firebaseapp.com",
+  databaseURL: "https://ahoranosotrasrd.firebaseio.com",
+  projectId: "ahoranosotrasrd",
+  storageBucket: "ahoranosotrasrd.appspot.com",
+  messagingSenderId: "223690789386",
+  appId: "1:223690789386:web:2ab8ee4d43516a19c545f3",
+  measurementId: "G-S0XZEG1JNP"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
