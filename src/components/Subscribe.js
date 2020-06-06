8bd714166412d9232bf5e165e6d7aee88e1a992a
puliv <pverapavez@gmail.com>
2020-06-06 19:10:20 -0400

    subscribe ok firebase

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -10,15 +10,33 @@ export class Subscribe extends Component {
     super(props)
 
     this.state = {
-      subscribeItem: [{
-        subsName: "",
-        subsEmail: "",
-        subsCity: "",
-        subsComment: "",
-        alert: false,
-        alertData: {}
-      }]
+      subsName: "",
+      subsEmail: "",
+      subsCity: "",
+      subsComment: "",
+      alert: false,
+      alertData: {}
     }
+  }
+
+  showAlert = (type, message) => {
+    this.setState({
+      alert: true,
+      alertData: { type, message }
+    });
+    setTimeout(() => {
+      this.setState({ alert: false });
+    }, 4000)
+  }
+
+  resetForm = () => {
+    // this.refs.contactForm.reset();
+    this.setState({
+      subsName: "",
+      subsEmail: "",
+      subsCity: "",
+      subsComment: ""
+    })
   }
 
   handleOnChange = (e) => {
@@ -65,34 +83,14 @@ export class Subscribe extends Component {
 
     if (subsName && subsEmail && subsCity && subsComment) {
       firebase.database().ref('subscriptions').push(subscribeItems).then(() => {
-        this.setState({
-          subsName: "",
-          subsEmail: "",
-          subsCity: "",
-          subsComment: "",
-          alert: true,
-          alertData: {
-            type: "success",
-            message: "Tu solicitud fue procesada con éxito."
-          }
-        })
+        this.showAlert("success", "Tu solicitud fue procesada con éxito.")
+        this.resetForm()
       }).catch(() => {
-        this.setState({
-          alert: true,
-          alertData: {
-            type: "fail",
-            message: "No se pudo procesar tu solicitud, intenta nuvamente."
-          }
-        })
+        this.showAlert("fail", "No se pudo procesar tu solicitud, intenta nuvamente.")
+        this.resetForm()
       })
     } else {
-      this.setState({
-        alert: true,
-        alertData: {
-          type: "fail",
-          message: "Debes completar todos los items del formulario."
-        }
-      })
+      this.showAlert("warning", "Debes completar todos los items.")
     }
   }
 
@@ -102,40 +100,41 @@ export class Subscribe extends Component {
     const { subsName, subsEmail, subsCity, subsComment, alert, alertData } = this.state
 
     return (
-      <div className="subscribe">
-        <div className="subscribe-div-alert">
-          <span className="subscribe-span">{alertData.message}</span>
+      <React.Fragment>
+        {alert && <div className={`subscribe-div-alert alert-${this.state.alertData.type}`}   >
+          <span className="subscribe-span-alert">{alertData.message}</span>
+        </div>}
+        <div className="subscribe">
+          <div className="subscribe-div">
+            <h2 className="subscribe-h2">Súmate!</h2>
+            <span className="subscribe-span">Suscríbete a nuestro newsletter</span>
+          </div>
+          <Form className="subscribe-form" required={true} ref="contactForm">
+            <div>
+              <h3 className="subscribe-h3">Nombre y Apellido</h3>
+              <Input onChange={this.handleOnChange} id="name" value={subsName} />
+            </div>
+            <div>
+              <h3 className="subscribe-h3">Correo</h3>
+              <Input onChange={this.handleOnChange} id="email" value={subsEmail} />
+            </div>
+            <div>
+              <h3 className="subscribe-h3">Ciudad/Comuna</h3>
+              <Input onChange={this.handleOnChange} id="city" value={subsCity} />
+            </div>
+            <div>
+              <h3 className="subscribe-h3">Comentarios</h3>
+              <TextArea onChange={this.handleOnChange} id="comment" value={subsComment} />
+            </div>
+            <div className="btn-div">
+              <Button
+                className="subscribe-btn"
+                type="primary"
+                onClick={this.handleClickSend}>Enviar</Button>
+            </div>
+          </Form>
         </div>
-        <div className="subscribe-div">
-          <h2 className="subscribe-h2">Súmate!</h2>
-          <span className="subscribe-span">Suscríbete a nuestro newsletter</span>
-        </div>
-        <Form className="subscribe-form" required={true}>
-          <div>
-            <h3 className="subscribe-h3">Nombre y Apellido</h3>
-            <Input onChange={this.handleOnChange} id="name" value={subsName} />
-          </div>
-          <div>
-            <h3 className="subscribe-h3">Correo</h3>
-            <Input onChange={this.handleOnChange} id="email" value={subsEmail} />
-          </div>
-          <div>
-            <h3 className="subscribe-h3">Ciudad/Comuna</h3>
-            <Input onChange={this.handleOnChange} id="city" value={subsCity} />
-          </div>
-          <div>
-            <h3 className="subscribe-h3">Comentarios</h3>
-            <TextArea onChange={this.handleOnChange} id="comment" value={subsComment} />
-          </div>
-          <div className="btn-div">
-            {alert && <span className="subscribe-span item">Debes rellenar todos los items</span>}
-            <Button
-              className="subscribe-btn"
-              type="primary"
-              onClick={this.handleClickSend}>Enviar</Button>
-          </div>
-        </Form>
-      </div>
+      </React.Fragment>
     )
   }
 }
